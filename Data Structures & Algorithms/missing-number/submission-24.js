f8a8c3aece92c824050aba5b7861c8d0ab300618
M.M.BAPIN <31760382+mmbapin@users.noreply.github.com>
2026-04-17 11:20:11 +0600

    Add: missing-number - submission-24

---
+++ Data Structures & Algorithms/missing-number/submission-24.js
@@ -0,0 +1,14 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    missingNumber(nums) {
+        let numSet = new Set(nums);
+        for(let i = 0; i < 32; i++){
+            if(!numSet.has(i)){
+                return i
+            }
+        }
+    }
+}
