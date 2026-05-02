75055cd15e41f7b006ecc759caefb1a7daba4918
M.M.BAPIN <31760382+mmbapin@users.noreply.github.com>
2026-05-02 14:05:10 +0600

    Add: validate-parentheses - submission-17

---
+++ Data Structures & Algorithms/validate-parentheses/submission-17.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        let newChar = [];
+        let pairs = {
+            ')': '(',
+            '}': '{',
+            ']': '['
+        }
+
+        for(let str of s){
+            if(['(', '{', '['].includes(str)){
+                newChar.push(str);
+            }else if([')', '}', ']'].includes(str)){
+                if(newChar.pop() !== pairs[str]){
+                    return false
+                }
+            }
+        }
+        return newChar.length === 0;
+    }
+}
